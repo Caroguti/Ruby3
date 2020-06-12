50f48239ff011b7f9d451792d3f7e063460090d7
Carolina Gutierrez <carolagutierrez@gmail.com>
2020-06-11 23:58:58 -0300

    Prueba1

--- Prueba/prueba.rb
+++ Prueba/prueba.rb
@@ -6,8 +6,8 @@ url="https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&ap
 api_key="2dvmRvfvQ7UPikh2ezZdFqWhAjUAeO3oYpuqFhum"
 
 
-def request_met(url, api_key)
-  url = URI(url+api_key)
+def request_met(link, key)
+  url = URI(link+key)
   https = Net::HTTP.new(url.host, url.port);
   https.use_ssl = true
   request = Net::HTTP::Get.new(url)
